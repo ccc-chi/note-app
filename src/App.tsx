962f84b9cc964c3f9880cf6ac69b7fc0ba43ae05
chihiro <chieeie@gmail.com>
2025-06-03 20:10:06 +0900

    sidebar途中

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+import { Main } from './components/Main';
+import { Sidebar } from './components/Sidebar';
+import  uuid  from 'react-uuid';
+
+type Note = {
+  id: string;
+  title: string;
+  content: string;
+  modDate: number;
+}
+
+function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: '新しいノート',
+      content: '新しいノートの内容です。',
+      modDate: Date.now(),
+    };
+    setNotes([...notes,newNote])
+    console.log(notes)
+  }
+
+  return (
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} notes={notes} />
+      <Main />
+    </div>
+  )
+}
+
+export default App
